96bee82f7afa3b8a31515724cef0cb3147b85e7a
叶长生 <x5brain@gmail.com>
2018-11-23 08:58:04 +0800

    scr

---
+++ lib/src/color.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+mixin color {
+
+  /// Register Page's Background Color
+  static final backgroundColor = Color.fromRGBO(232, 236, 239, 1);
+
+  /// Register Page's title Color
+  static final titleColor = Color.fromRGBO(83, 106, 176, 1);
+
+  /// Register Page's font Color
+  static final fontColor = Color.fromRGBO(91, 95, 98, 1);
+
+  /// Register Page's button Color
+  static final buttonColor = Color.fromRGBO(100, 147, 252, 1);
+
+}
